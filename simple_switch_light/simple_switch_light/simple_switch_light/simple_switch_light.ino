99eb53e14a7e9556cbd126f51a5e06a94e77fa3e
Никита Пермяков <permikov134@mail.ru>
2021-07-26 00:23:30 +0300

    switch light

---
+++ simple_switch_light/simple_switch_light/simple_switch_light/simple_switch_light.ino
@@ -0,0 +1,28 @@
+int switchPin = 8;
+int ledPin = 13; 
+boolean lastBtn = LOW;
+boolean currentBtn = LOW;
+boolean led0n = false;
+
+void setup() {
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+boolean debounce(boolean last){
+  boolean current = digitalRead(switchPin);
+  if (last != current) {
+    delay(5);
+    current = digitalRead(switchPin); 
+  }
+  return current;
+}
+
+void loop() {
+  currentBtn = debounce(lastBtn); 
+  if (lastBtn == LOW && currentBtn == HIGH){
+    led0n = !led0n;
+  }
+  lastBtn = currentBtn;
+  digitalWrite(ledPin, led0n);
+}
